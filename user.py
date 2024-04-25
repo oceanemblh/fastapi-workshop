566483df33c125628fe1aa9f51cd99376ce201f4
Océane <oceane.mabialah@reseau.eseo.fr>
2024-04-25 16:23:32 +0200

    Ajout de la méthode de mise à jour des données d'un utilisateur

--- user.py
+++ user.py
@@ -39,9 +39,28 @@ def get_all_users(db: Session = Depends(get_db)):
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found") # Catch de l'exception si l'utilisateur est introuvable
     return user
 
 # Update an existing user:
+
+@route.put("/users/{user_id}")
+def update_user(user_id: int, request: schemas.User, db: Session = Depends(get_db)):
+    # Récupération de l'utilisateur dans base de données
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found") # Catch de l'exception si l'utilisateur est introuvable
+    
+    # Mise à jour des champs de l'utilisateur avec les nouvelles données
+    for field, value in request.dict().items():
+        setattr(user, field, value)
+    
+    # Mise à jour des modifications dans la base de données
+    db.commit()
+    
+    # Rafraîchir l'objet utilisateur dans la session
+    db.refresh(user)
+    
+    return user
 
 # delete an existing user:
